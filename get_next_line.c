d9d94b446aa1cbf6a6f6caba2ce0a7cfdae87de2
Christiaan Herrewijn <cherrewi@f0r3s3.codam.nl>
2022-11-10 14:07:41 +0100

    condensed function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/30 14:34:02 by cherrewi      #+#    #+#                 */
-/*   Updated: 2022/11/09 17:14:55 by cherrewi      ########   odam.nl         */
+/*   Updated: 2022/11/10 14:05:19 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,24 @@ char	*get_next_line(int fd)
 {
 	static char	*fd_part = NULL;
 	char		*line_str;
-	int			i_linebreak;
 	int			final_read_len;
 
 	line_str = NULL;
-	i_linebreak = get_line_break(fd_part);
-	if (i_linebreak == -1)
+	if (get_line_break(fd_part) == -1)
 	{		
 		final_read_len = read_from_file(fd, &fd_part);
-		i_linebreak = get_line_break(fd_part);
 		if (final_read_len >= 0
-			&& final_read_len < BUFFER_SIZE && i_linebreak == -1)
+			&& final_read_len < BUFFER_SIZE && get_line_break(fd_part) == -1)
 		{
 			line_str = create_line_str(fd_part, gnl_strlen(fd_part));
 			free(fd_part);
 			fd_part = NULL;
 		}
 	}
-	if (i_linebreak != -1)
+	if (get_line_break(fd_part) != -1)
 	{
-		line_str = create_line_str(fd_part, i_linebreak + 1);
-		fd_part = realloc_fd_part(fd_part, i_linebreak + 1, 0);
+		line_str = create_line_str(fd_part, get_line_break(fd_part) + 1);
+		fd_part = realloc_fd_part(fd_part, get_line_break(fd_part) + 1, 0);
 	}
 	return (line_str);
 }
